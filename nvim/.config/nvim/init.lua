98fb276f059aa877db65daf1e6b23865ddad5300
Cameron Harder-Hutton <cameron@harderhutton.dev>
2025-12-14 11:28:51 +0800

    feat: show relative file paths in Neovim statusline
    - Override mini.statusline filename section to display paths relative to cwd
    - Reduces visual clutter by showing only relevant path segments from project root
    - Maintains context while eliminating unnecessary absolute path information
    - Improves readability when working within project directories

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -989,9 +989,13 @@ require('lazy').setup({
       -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
 
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
+      -- Show filepath relative to cwd
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_filename = function()
+        return vim.fn.fnamemodify(vim.fn.expand '%', ':.')
+      end
+
+      -- Cursor location as LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
         return '%2l:%-2v'
