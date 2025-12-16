4b9e7a1b7552ef1f74bd2fb4c92f47f250e64759
Cameron Harder-Hutton <cameron@harderhutton.dev>
2025-12-17 02:28:04 +1000

    feat: enhance oil.nvim configuration with hidden files and auto-save
    - Enable show_hidden to display dotfiles and hidden directories by default
    - Add prompt_save_on_select_new_entry to prevent data loss when navigating
    - Improve file management workflow by making hidden files accessible
    - Auto-prompt saves buffer changes when switching to new files/directories

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,10 @@
 return {
     "stevearc/oil.nvim",
-    opts = { win_options = { signcolumn = "yes:2" } },
+    opts = {
+        win_options = { signcolumn = "yes:2" },
+        view_options = { show_hidden = true },
+        prompt_save_on_select_new_entry = true,
+    },
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     lazy = false,
 }
